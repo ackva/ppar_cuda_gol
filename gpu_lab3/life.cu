b977c2f58872a565b7bf501706fcf49b823ff499
Martin POTEL <martinpotel@dn056091.etu.univ-rennes1.fr>
2018-04-13 18:16:45 +0200

    last version

--- gpu_lab3/life.cu
+++ gpu_lab3/life.cu
@@ -1,23 +1,8 @@
-/* POTEL Martin
-   ACKVA Adrian */
-
 
 #include "utils.h"
 #include <stdlib.h>
-#include <getopt.h>
-#include <ctype.h>
-#include <unistd.h>
 
 #include "life_kernel.cu"
-
-#define WARP_SIZE 32
-#define DEFAULT_DIM_X 128
-#define DEFAULT_DIM_Y 128
-#define DEFAULT_STEPS 2
-#define DEFAULT_CELLS_PER_WORD 1
-
-#define MIN(x,y) ((x < y) ? x : y)
-#define MAX(x,y) ((x < y) ? y : x)
 
 void init_data(int * domain, int domain_x, int domain_y)
 {
@@ -37,13 +22,13 @@ void print_domain(int* domain, int domain_x, int domain_y, int* red, int* blue) 
 			int cell = domain[y * domain_x + x];
 			switch(cell) {
 				case 0:
-					printf("\033[40m  \033[0m");
+					printf("\033[40m  \033[0m"); //EMPTY
 					break;
 				case 1:
-					printf("\033[41m  \033[0m");
+					printf("\033[41m  \033[0m"); //RED
 					break;
 				case 2:
-					printf("\033[44m  \033[0m");
+					printf("\033[44m  \033[0m"); //BLUE
 					break;
 				default:
 					break;
@@ -58,87 +43,22 @@ void print_domain(int* domain, int domain_x, int domain_y, int* red, int* blue) 
 	}
 }
 
-/*
- * Calculates the grid and blocks dimensions
- *
- * @param domain_x:     width of the domain
- * @param domain_y:     height of the domain
- * @param blockDim_x:   destination for the returned blocks dimensions along the x axis
- * @param blockDim_y:   destination for the returned blocks dimensions along the y axis
- * @param gridDim_x:    destination for the returned grid's dimensions along the x axis
- * @param gridDim_x:    destination for the returned grid's dimensions along the y axis
- */
-void calculate_grid (int domain_x, int domain_y,
-    int *blockDim_x, int *blockDim_y, int *gridDim_x, int *gridDim_y)
-{
-    //  32x32 blocks
-    *blockDim_x = WARP_SIZE;
-    *blockDim_y = WARP_SIZE;
-
-    *gridDim_x = domain_x / *blockDim_x;
-    *gridDim_y = domain_y / *blockDim_y;
-}
-
 int main(int argc, char ** argv)
 {
-    printf ("Game of Life starting ...\n");
-
     // Definition of parameters
-    int domain_x = DEFAULT_DIM_X;
-    int domain_y = DEFAULT_DIM_Y;
-    int cells_per_word = DEFAULT_CELLS_PER_WORD;
-    int steps = DEFAULT_STEPS;
-
-    int c, cval;
-    while ((c = getopt (argc, argv, "x:y:s:")) != -1)
-    {
-        switch (c)
-        {
-            case 'x':
-                if ((cval = atoi (optarg)) % WARP_SIZE == 0 && cval > 0)
-                    domain_x = cval;
-                else
-                    fprintf (stderr, 
-                        "Invalid domain size '%s' : \
-                        dimension_x must be a positive multiple of warp size (%d).\n", optarg, WARP_SIZE);
-                break;
-            case 'y':
-                if ((cval = atoi (optarg)) % WARP_SIZE == 0 && cval > 0)
-                    domain_y = cval;
-                else
-                    fprintf (stderr, 
-                        "Invalid domain size '%s' : \
-                        dimension_y must be a positive multiple of warp size (%d).\n", optarg, WARP_SIZE);
-                break;
-            case 's':
-                if ((cval = atoi (optarg)) > 0)
-                    steps = cval;
-                else
-                    fprintf (stderr,
-                        "Invalid number of steps '%s' :\
-                        must be a positive integer.\n", optarg);
-                break;
-        }
-    }
-
-    printf ("\nUsing domain dimensions : %d x %d\n", domain_x, domain_y);
-
-    /*
-    int blocks_x = 1;
-    int threads_per_block = 1024;
-    c = MAX (1, threads_per_block / domain_x);
-    int blocks_y = domain_y / c;
-    */
-    int blockDim_x, blockDim_y, gridDim_x, gridDim_y;
-    calculate_grid (domain_x, domain_y, &blockDim_x, &blockDim_y, &gridDim_x, &gridDim_y);
-
-    // CUDA grid dimensions
-    dim3  grid (gridDim_x, gridDim_y);
-    // CUDA block dimensions
-    dim3  threads (blockDim_x, blockDim_y);
-
-    printf ("\nUsing grid dimensions :\t\t%d x %d\
-        \n\tblock dimensions :\t%d x %d\n", grid.x, grid.y, threads.x, threads.y);
+    int domain_x = 128;	// Multiple of threads_per_block * cells_per_word
+    int domain_y = 128;
+    
+    int cells_per_word = 1;
+    
+    int steps = 5;
+    
+    int threads_per_block = 128;
+    int blocks_x = domain_x / (threads_per_block * cells_per_word);
+    int blocks_y = domain_y;
+    
+    dim3  grid(blocks_x, blocks_y);	// CUDA grid dimensions
+    dim3  threads(threads_per_block);	// CUDA block dimensions
 
     // Allocation of arrays
     int * domain_gpu[2] = {NULL, NULL};
@@ -148,19 +68,11 @@ int main(int argc, char ** argv)
 	CUDA_SAFE_CALL(cudaMalloc((void**)&domain_gpu[0], domain_size));
     CUDA_SAFE_CALL(cudaMalloc((void**)&domain_gpu[1], domain_size));
 
-    int * domain_cpu = (int*) malloc(domain_size);
+    int * domain_cpu = (int*)malloc(domain_size);
 
 	// Arrays of dimensions pitch * domain.y
 
 	init_data(domain_cpu, domain_x, domain_y);
-
-    // Count colors
-    int red = 0;
-    int blue = 0;
-    printf ("Original domain :\n");
-    print_domain(domain_cpu, domain_x, domain_y, &red, &blue);
-
-    //  Copy data to device global memory
     CUDA_SAFE_CALL(cudaMemcpy(domain_gpu[0], domain_cpu, domain_size, cudaMemcpyHostToDevice));
 
     // Timer initialization
@@ -171,15 +83,10 @@ int main(int argc, char ** argv)
     // Start timer
     CUDA_SAFE_CALL(cudaEventRecord(start, 0));
 
-    //  Compute the necessary shared memory size
-    //int sm_x = MIN(blocks_x + 2, domain_x);
-    //int sm_y = MIN(blocks_y + 2, domain_y);
-    //int shared_mem_size = sm_x * sm_y * sizeof(int);
-
     // Kernel execution
+    int shared_mem_size = 0;
     for(int i = 0; i < steps; i++) {
-	    //life_kernel<<< grid, threads, shared_mem_size >>>(domain_gpu[i%2],
-        life_kernel<<< grid, threads >>>(domain_gpu[i%2],
+	    life_kernel<<< grid, threads, shared_mem_size >>>(domain_gpu[i%2],
 	    	domain_gpu[(i+1)%2], domain_x, domain_y);
 	}
 
@@ -202,6 +109,8 @@ int main(int argc, char ** argv)
     
 
     // Count colors
+    int red = 0;
+    int blue = 0;
     print_domain(domain_cpu, domain_x, domain_y, &red, &blue);
     printf("Red/Blue cells: %d/%d\n", red, blue);
     
--- gpu_lab3/life_kernel.cu
+++ gpu_lab3/life_kernel.cu
@@ -1,139 +1,70 @@
-#define BLOCK_DIM_X 32
-#define BLOCK_DIM_Y 32
 
 // Reads a cell at (x+dx, y+dy)
 __device__ int read_cell(int * source_domain, int x, int y, int dx, int dy,
     unsigned int domain_x, unsigned int domain_y)
 {
-    //  handle negative offsets
-    if (dx < 0)
-        x = (unsigned int) domain_x + dx;
-    else
-        x = (unsigned int)(x + dx) % domain_x;	// Wrap around
-
-    if (dy < 0)
-        y = (unsigned int) domain_y + dy;
-    else
-        y = (unsigned int)(y + dy) % domain_y;
-
+    x = (unsigned int)(x + dx) % domain_x;	// Wrap around
+    y = (unsigned int)(y + dy) % domain_y;
     return source_domain[y * domain_x + x];
-}
-
-/*
- *  Read neighbor cells' values
- *
- *  @param subdomain:   shared memory space
- *  @param txr:         thread's Id relative to the block's x dimension
- *  @param tyr:         thread's Id relative to the block's y dimension
- *  
- *  @param self:        destination for the cell's value
- *  @param alive:       destination for the cell's live neighbor count
- *  @param blue:        destination for the cell's blue neighbor count
- *
- */
-__device__ void read_values (int subdomain[BLOCK_DIM_Y+2][BLOCK_DIM_X+2], int txr, int tyr,
-    int *self, int *alive, int *blue)
-{
-    int self_ = 0, alive_ = 0, blue_ = 0, current;
-
-    for (int y_offset = 0 ; y_offset < 3
-                                && (self > 0 || alive_ < 4) ; y_offset++)
-    {
-        for (int x_offset = 0 ; x_offset < 3
-                                && (self_ > 0 || alive_ < 4) ; x_offset++)
-        {
-            if (y_offset == 1 && x_offset == 1)
-                self_ = subdomain[tyr + 1][txr + 1];
-            else
-            {
-                current = subdomain[tyr + y_offset][txr + x_offset];
-                alive_ += current;
-
-                if (current == 2)
-                {
-                    alive_--;
-                    blue_++;
-                }
-            }
-        }
-    }
-
-    *self = self_;
-    *alive = alive_;
-    *blue = blue_;
-}
-
-/*
- *  Compute the cell's next value
- *
- *  @param current: current value
- *  @param alive:   number of alive neighbor cells
- *  @param blue:    number of blue neighbor cells
- *
- *  @return:        the cell's next value
- */
-__device__ int new_value (int current, int alive, int blue)
-{
-    //  die cases
-    if (alive > 3 || alive < 2)
-        return 0;
-
-    //  empty cell going alive
-    if (current == 0 && alive == 3)
-        //  majority of red neighbors
-        if (blue < 2)
-            return 1;
-        //  majority of blue neighbors
-        else
-            return 2;
-
-    //  else survive or stay dead
-    return current;
 }
 
 // Compute kernel
 __global__ void life_kernel(int * source_domain, int * dest_domain,
     int domain_x, int domain_y)
 {
-    //  thread IDs relative to the block
-    int tx_r = threadIdx.x;
-    int ty_r = threadIdx.y;
-
-    //  thread IDs : absolute
-    int tx = blockIdx.x * blockDim.x + tx_r;
-    int ty = blockIdx.y * blockDim.y + ty_r;
-
-    //  shared memory space
-    __shared__ int subdomain[BLOCK_DIM_Y + 2][BLOCK_DIM_X + 2];
+    int tx = blockIdx.x * blockDim.x + threadIdx.x;
+    int ty = blockIdx.y;
     
-    /*  
-     *  Load values in shared memory
-     */
+    // Read cell
+    int myself = read_cell(source_domain, tx, ty, 0, 0, domain_x, domain_y);
+     
+    // TODO: Read the 8 neighbors and count number of blue and red
+    int neighbors[8];
+    neighbors[0] = read_cell(source_domain, tx, ty,  0, -1, domain_x, domain_y);
+    neighbors[1] = read_cell(source_domain, tx, ty,  0,  1, domain_x, domain_y);
+    neighbors[2] = read_cell(source_domain, tx, ty, -1, -1, domain_x, domain_y);
+    neighbors[3] = read_cell(source_domain, tx, ty, -1,  0, domain_x, domain_y);
+    neighbors[4] = read_cell(source_domain, tx, ty, -1,  1, domain_x, domain_y);
+    neighbors[5] = read_cell(source_domain, tx, ty,  1, -1, domain_x, domain_y);
+    neighbors[6] = read_cell(source_domain, tx, ty,  1,  0, domain_x, domain_y);
+    neighbors[7] = read_cell(source_domain, tx, ty,  1,  1, domain_x, domain_y);
+	
+    // TODO: Compute new value
 
-    //  first step : every thread reads its cell's upper-left neighbor
-    subdomain[ty_r][tx_r] = read_cell (source_domain, tx, ty, -1, -1, domain_x, domain_y);
+    int blue = 0, red = 0, empty = 0;
 
-    /*  second step : 
-     *  - the last two rows of threads read their bottom-left neighbor
-     */
-    if (ty_r >= BLOCK_DIM_Y - 2)
-        subdomain[ty_r + 2][tx_r] = read_cell (source_domain, tx, ty, -1, 1, domain_x, domain_y);
+	int myselfNew = 0;
+    for (int i = 0; i < 8; i++) {
+        switch(neighbors[i]) {
+          case 0 :
+             empty++;
+             break;
+          case 1 :
+             red++;
+             break;
+          case 2 :
+             blue++;
+             break;
+          default :
+             break;
+    }
+    }
 
-    /*  third step :
-     *  - the last two columns of threads read their upper-right neighbor
-     */
-    if (tx_r >= BLOCK_DIM_X - 2)
-        subdomain[ty_r][tx_r + 2] = read_cell (source_domain, tx, ty, 1, -1, domain_x, domain_y);
+    int alive = red + blue;
 
-	/*
-     *  Read neighbor cell's values
-     */
-    int myself, alive, blue;
-    read_values (subdomain, tx_r, ty_r, &myself, &alive, &blue);
+    if (alive < 2 || alive > 3)
+        myselfNew = 0;
+    else if (alive == 2)
+        myselfNew = myself;
+    else {
+        if (red > blue)
+            myselfNew = 1;
+        else
+            myselfNew = 2;
+    }
 
-	// Write it in dest_domain
-    dest_domain[ty * domain_x + tx] = new_value (myself, alive, blue);
+	// TODO: Write it in dest_domain
+    dest_domain[ (ty * domain_x) + tx] = myselfNew;
 
-    __syncthreads();
 }
 
